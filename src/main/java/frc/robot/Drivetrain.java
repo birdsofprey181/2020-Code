148ab9e6864df92f01b6100464dc000413959a29
Gwen Miller <gwenmillerpathways@gmail.com>
2020-01-11 11:21:06 -0500

    Added basic spark motors and P loop

--- src/main/java/frc/robot/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain.java
@@ -1,5 +1,44 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 public class Drivetrain{
+
+    private DifferentialDrive drive;
+    private CANSparkMax motorL, motorR;
+    private static final int leftID = 1; 
+    private static final int rightID = 2;
     
+    motorL=new CANSparkMax(leftID, MotorType.kBrushless);
+    motorR=new CANSparkMax(rightID, MotorType.kBrushless);
+
+    public void resetEnc(int port){
+        //reset encoder in x port
+    }
+    public double calcDist(){
+        double distTrav=0;
+        double encRot=0;
+        double wheelCon=0;
+        //set wheel constant to distance traveled per 1 encoder rotation
+        //encRot = read encoder rotations;
+        distTrav=encRot*wheelCon;
+        return(distTrav);
+    }
+    public void pidExample(double totDist){
+        //kP (%speed per error)
+        //totDist (how far we want to travel)
+        //motorOut(kP*(totDist-calcDist()));
+
+    }
+    public void pidExample(double totDist, double kP){
+        //kP (%speed per error)
+        //totDist (how far we want to travel)
+        //motorOut(kP*(totDist-calcDist()));
+
+    }
 }
