673b7fb7234e2c9085d8491162e3c12be8912bf9
Gwen Miller <gwenmillerpathways@gmail.com>
2020-02-25 19:19:03 -0500

    added lock value

--- src/main/java/frc/robot/Elevator.java
+++ src/main/java/frc/robot/Elevator.java
@@ -1,11 +1,14 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.Relay;
 
 public class Elevator{
 
     static VictorSP eleMotorL=new VictorSP(2);
     static VictorSP eleMotorR=new VictorSP(3);
+
+    static Relay lock=new Relay(0);
 
     public static void controlEle(boolean b1, double d1){
         if(b1){
@@ -14,6 +17,14 @@ public class Elevator{
         }else{
             eleMotorL.set(0);
             eleMotorR.set(0);
+        }
+    }
+    
+    public static void controlLock(boolean b1){
+        if(b1){
+            lock.set(Relay.Value.kForward);
+        }else{
+            lock.set(Relay.Value.kOff);
         }
     }
 }
